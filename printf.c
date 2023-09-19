89b2f542223d4f54cd9750cc2e58fc5a52656285
Dairo Stephen Olusola <dairostephen@outlook.com>
2023-09-19 17:48:05 +0100

    Updated printf

--- printf.c
+++ printf.c
@@ -1,44 +1,45 @@
-#include <limits.h>
+/*
+ * Printf -Function print via pring
+ * Strlen - No of letters
+ *
+ */
+
 #include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
+int _printf(const char *format, ...) 
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+    va_list args;
+    va_start(args, format);
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (len);
+    int char_count = 0;
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++; // Move past the '%'
+            if (*format == '\0') break; // Handle format string ending with '%'
+            if (*format == 'c') {
+                int c = va_arg(args, int);
+                putchar(c);
+                char_count++;
+            } else if (*format == 's') {
+                char *s = va_arg(args, char *);
+                while (*s != '\0') {
+                    putchar(*s);
+                    s++;
+                    char_count++;
+                }
+            } else if (*format == '%') {
+                putchar('%');
+                char_count++;
+            }
+        } else {
+            putchar(*format);
+            char_count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return char_count;
 }
